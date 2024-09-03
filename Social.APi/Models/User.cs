376010294633c8638cb0f3b77ada15a8bf4674b3
Ariyanayagan <ariyanayagan@cygnussoftwares.com>
2024-09-03 15:46:39 +0530

    Changes

--- Social.APi/Models/User.cs
+++ Social.APi/Models/User.cs
@@ -7,6 +7,9 @@
         public string Email { get; set; }
         public string PasswordHash { get; set; } // Storing password securely
 
+        public string? PasswordResetToken { get; set; }
+        public DateTime? PasswordResetTokenExpiry { get; set; }
+
         // Navigation properties
         public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
         public ICollection<FriendRequest> SentFriendRequests { get; set; } = new List<FriendRequest>();
